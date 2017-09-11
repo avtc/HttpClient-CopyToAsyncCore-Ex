7b4d6c14194265570cc7f206e13712ee4a945c14
Olexandr Tarasenkov <olexandr@leadsmarket.com>
2017-09-11 16:21:55 +0300

    Initial commit

---
+++ SampleApp/Controllers/ValuesController.cs
@@ -0,0 +1,120 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "healthy";
+        }
+
+        // GET api/values/fake/0
+        [HttpGet]
+        [HttpPost]
+        [Route("fake/{length:int}")]
+        public async Task<string> GetFake(int length)
+        {
+            await Task.Delay(100);
+            return new string('A', length);
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("post")]
+        public async Task<string> PostToPostAsync([FromBody]string url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Connection.Clear();
+                client.DefaultRequestHeaders.ConnectionClose = true;
+                using (var response = await client.PostAsync(
+                    url, 
+                    new StringContent(url, Encoding.UTF8, "application/json")).ConfigureAwait(false))
+                {
+                    try
+                    {
+                        var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        return result;
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(new EventId(ex.HResult), ex, ex.Message);
+                        return "Ex: " + ex.ToString();
+                    }
+                }
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Route("runpost")]
+        public string PostToRunPostAsync([FromBody]string url)
+        {
+            var t = Task.Run(async() =>
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Connection.Clear();
+                    client.DefaultRequestHeaders.ConnectionClose = true;
+                    using (var response = await client.PostAsync(
+                        url,
+                        new StringContent(url, Encoding.UTF8, "application/json")).ConfigureAwait(false))
+                    {
+                        try
+                        {
+                            var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            return result;
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(new EventId(ex.HResult), ex, ex.Message);
+                            return "Ex: " + ex.ToString();
+                        }
+                    }
+                }
+            });
+            return "accepted";
+        }
+
+        // POST api/values/get
+        [HttpPost]
+        [Route("get")]
+        public async Task<string> PostToGetAsync([FromBody]string url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Connection.Clear();
+                client.DefaultRequestHeaders.ConnectionClose = true;
+                using (var response = await client.GetAsync(url).ConfigureAwait(false))
+                {
+                    try
+                    {
+                        var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        return result;
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(new EventId(ex.HResult), ex, ex.Message);
+                        return "Ex: " + ex.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SampleApp/Services/WarmupService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+
+namespace SampleApp.Services
+{
+    public class WarmupService
+    {
+        public WarmupService(
+            IApplicationLifetime appLifetime)
+        {
+            if (appLifetime == null) throw new ArgumentNullException(nameof(appLifetime));
+            appLifetime.ApplicationStarted.Register(async () => await WarmupAsync().ConfigureAwait(false));
+        }
+
+        public async Task WarmupAsync()
+        {
+            const int cnt = 500;
+            const int payloadLength = 1000;
+            var payload = new string('B', payloadLength);
+
+            Console.WriteLine("Warmup...");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            string url = Environment.GetEnvironmentVariable("ASPNETCORE_URLS");
+            if (string.IsNullOrEmpty(url) || url == "http://localhost:50614/")
+                url = "http://localhost:50614";
+            else
+                url = "http://localhost:80";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Connection.Clear();
+                client.DefaultRequestHeaders.ConnectionClose = true;
+                //client.DefaultRequestHeaders.Connection.Add("keep-alive");
+                //client.DefaultRequestHeaders.ConnectionClose = false;
+                //client.DefaultRequestHeaders.Connection.Add("keep-alive");
+
+                // call endpoints
+                for (int i = 0; i < cnt; i++)
+                {
+                    try
+                    {
+                        var fakeurl = $"{url}/api/values/fake/{payloadLength}";
+                        using (var response = await client.PostAsync(
+                            $"{url}/api/values/runpost",
+                            new StringContent("\"" + fakeurl + "\"", Encoding.UTF8, "application/json")).ConfigureAwait(false))
+                        {
+                            var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            if (result.StartsWith("Ex: "))
+                            {
+                                Console.WriteLine(result);
+                                break;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        var error = $"Warming up error for #{i}: {ex.ToString()}";
+                        Console.WriteLine(error);
+                        break;
+                    }
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"Warmup took: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
